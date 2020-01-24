1c7459c30cb2569bec7eb42abd0a3a60f29b801c
Rinald Shabani <rinaldshabani@gmail.com>
2020-01-24 19:31:04 +0100

    Add sound effects

--- engine/sprite_sheet.py
+++ engine/sprite_sheet.py
@@ -1,4 +1,4 @@
-from . import pg_image
+from . import pg_image, pg_transform
 
 
 class SpriteSheet:
@@ -6,5 +6,24 @@ class SpriteSheet:
         self.image = pg_image.load(image_path)
         self.tiles = tiles
 
-    def tile(self, tile_id):
-        return self.image.subsurface(self.tiles[tile_id])
+    def tile(self, tile_id, size=(32, 32)):
+        image = self.image.subsurface(self.tiles[tile_id])
+
+        # if flip:
+        #     image = pg_transform.flip(image, 1, 0)
+        image = pg_transform.scale(image, size)
+
+        return image
+
+    def animation_tiles(self, tile_id, size=(32, 32)):
+        images = []
+        for rect in self.tiles[tile_id]:
+            image = self.image.subsurface(rect)
+
+            # if flip:
+            #     image = pg_transform.flip(image, 1, 0)
+            image = pg_transform.scale(image, size)
+
+            images.append(image)
+
+        return images
